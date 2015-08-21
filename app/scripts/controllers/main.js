7c8ad3b1a93509c611e35e6dce314eb3ced1534c
Ashlyn <ashlyn.aiu@gmail.com>
2015-08-20 19:35:03 -0700

    fixed delete function.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,9 +11,11 @@ angular.module('notesApp')
   .controller('MainCtrl', function ($scope, notesData) {
     $scope.notes = notesData;
     $scope.toogleModel = false;
-    
+
     $scope.delete = function(index) {
+        $scope.notes.reverse();
         $scope.notes.splice(index, 1);
+        $scope.notes.reverse();
     };
     $scope.toogleButton = function() {
         if ($scope.toogleModel === false) {
